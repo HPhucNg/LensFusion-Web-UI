c1eb0ef96b4dc20228e5e6ebfc1495988e68fb2c
katiemartinezz <119619811+katiemartinezz@users.noreply.github.com>
2024-11-25 14:13:10 -0800

    started on price page

--- src/app/(list_page)/pricing/page.jsx
+++ src/app/(list_page)/pricing/page.jsx
@@ -1,8 +1,20 @@
+'use client'
 import React from 'react'
+import Navbar from '@/components/Navbar';
+import '../style.css';
 
 function page() {
   return (
-    <div>page</div>
+    <>
+    <Navbar />
+    <h1 className='priceh1'>How much is your time worth?</h1>
+    <div>
+      <p>Pricing cards here</p>
+    </div>
+    <div>
+      <h2>Frequently asked questions</h2>
+    </div>
+    </>
   )
 }
 
