dcffe7426d93851ab2618eaee28fd8fb450410c9
Peter Herrera <peter.herrera01@student.csulb.edu>
2024-12-03 20:55:39 -0800

    Navbar formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,25 +12,40 @@ function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="top-0 w-full z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-5 px-4">
+      <header className=" top-0 w-full z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-5 px-4">
       <nav className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 border-slate-900 rounded-full border shadow-xl border-gray-800">
         <div className="flex items-center ml-5">
-          <Branding />
+            <Branding/>
         </div>
         <div className="hidden md:flex space-x-4">
-          <Link href="/solutions" className="text-slate-400 hover:text-white transition-colors">
+            <Link
+              href="/solutions"
+              className="text-slate-400 hover:text-white transition-colors"
+            >
             Solutions
           </Link>
-          <Link href="/resources" className="text-slate-400 hover:text-white transition-colors">
+            <Link
+              href="/resources"
+              className="text-slate-400 hover:text-white transition-colors"
+            >
             Resources
           </Link>
-          <Link href="/community" className="text-slate-400 hover:text-white transition-colors">
+            <Link
+              href="/community"
+              className="text-slate-400 hover:text-white transition-colors"
+            >
             Community
           </Link>
-          <Link href="/pricing" className="text-slate-400 hover:text-white transition-colors">
+            <Link
+              href="/pricing"
+              className="text-slate-400 hover:text-white transition-colors"
+            >
             Pricing
           </Link>
-          <Link href="/contact" className="text-slate-400 hover:text-white transition-colors">
+            <Link
+              href="/contact"
+              className="text-slate-400 hover:text-white transition-colors"
+            >
             Contact
           </Link>
         </div>
