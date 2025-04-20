8c2183c02ba5ce71af9f3e119a81e0900985ef91
Phuc Nguyen <phucnguyen0331@gmail.com>
2025-04-20 03:36:27 -0700

    add workspace navbar for all AI feature

--- src/app/(list_page)/workspace/backgroundgeneration/page.jsx
+++ src/app/(list_page)/workspace/backgroundgeneration/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 import BackButton from '@/components/BackButton';
 import ImageProcessor from './_components/ImageProcessor';
+import WorkspaceNavbar from '@/components/WorkspaceNavbar';
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <BackButton /> {/* Use the BackButton component */}
+     <WorkspaceNavbar/>
 
       <h1 className="text-2xl font-bold mb-6">
         Image Processing with HuggingFace
--- src/app/(list_page)/workspace/objectRetouch/page.jsx
+++ src/app/(list_page)/workspace/objectRetouch/page.jsx
@@ -1,12 +1,13 @@
 "use client";
 import BackButton from '@/components/BackButton';
 import Inpaint from './_components/Inpaint';
+import WorkspaceNavbar from '@/components/WorkspaceNavbar';
 
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <BackButton /> {/* Use the BackButton component */}
+      <WorkspaceNavbar/>
 
       <h1 className="text-2xl font-bold mb-6">
         Object Retouch Tool
