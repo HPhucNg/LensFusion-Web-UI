c95e0d4245ecd2cfe29df57b33b5fecce5314f78
Rayna <rayna2335@gmail.com>
2025-05-10 12:23:34 -0700

    remove navbar and centered

--- src/app/payment/cancel/page.js
+++ src/app/payment/cancel/page.js
@@ -1,7 +1,6 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Navbar from '@/components/Navbar';
 
 //displays after user cancels the subscriptions
 export default function Cancel() {
@@ -17,8 +16,7 @@ export default function Cancel() {
   }, [router]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black text-white">
-      <Navbar />
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black text-white flex items-center justify-center">
       <div className="flex items-center justify-center">
         <div className="text-center p-8 rounded-xl shadow-2xl border border-gray-800">
           <h1 className="text-3xl font-bold mb-4">Subscription canceled.</h1>
--- src/app/payment/success/page.js
+++ src/app/payment/success/page.js
@@ -1,8 +1,6 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 
 //displays when user successfully pay for the subscription
 export default function Success() {
@@ -10,15 +8,14 @@ export default function Success() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/dashboard/profile');
+      router.push('/dashboard');
     }, 5000);
 
     return () => clearTimeout(timer);
   }, [router]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black text-white">
-      <Navbar />
+    <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black text-white flex items-center justify-center">
       <div className="flex items-center justify-center">
         <div className="text-center p-8 ">
           <h1 className="text-3xl font-bold mb-4">Payment Successful!</h1>
